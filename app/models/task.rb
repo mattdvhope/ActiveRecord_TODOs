a1e03b856d24b3ea3c2630f8d97500e1142b9179
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-13 16:24:38 -0500

    with ARGV mods

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,17 @@
 class Task < ActiveRecord::Base
   belongs_to :list
+
+  def print_all
+   <<-string
+   TASK 
+   ID: #{id}
+   TODO: #{todo}
+   string
+  end
+  def self.list
+    Task.all.select do |t|
+      puts t.print_all
+    end
+  end
+
 end
--- todo.rb
+++ todo.rb
@@ -1,3 +1,18 @@
 require_relative 'config/application'
 
-puts "Put your application code in #{File.expand_path(__FILE__)}"
+
+if ARGV.any?
+
+  case ARGV[0]
+  when 'list'
+    p Task.list
+  when 'delete'
+      var = Task.find(ARGV[1])
+      var.delete
+  end
+
+  # var = Task.id = ARGV[1]
+  # var.d
+
+end
+# puts "Put your application code in #{File.expand_path(__FILE__)}"
